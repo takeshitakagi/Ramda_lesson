9e9b88d0e4dc668ea88b6f59f9f362d6f0e5c05a
takeshi <takagitakeshi2@gmail.com>
2019-06-12 00:06:41 +0900

    second commit

--- src/RamudaTest.java
+++ src/RamudaTest.java
@@ -1,23 +1,35 @@
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class RamudaTest {
 
 	public static void main(String[] args) {
 
-		Map<Integer, String> users = new HashMap<Integer, String>();
-		users.put(1, "佐藤A男");
-		users.put(2, "鈴木B子");
-		users.put(3, "高橋C助");
+		User user1 = new User(1, "佐藤A男");
+		User user2 = new User(2, "鈴木B子");
+		User user3 = new User(3, "高橋C助");
+
+		List<User> users = new ArrayList<>();
+
+		users.add(user1);
+		users.add(user2);
+		users.add(user3);
+
+		for(User user : users) {
+			System.out.print(user.getId() + "：");
+			System.out.println(user.getName());
+		}
+
+		testMethod(User users);
+
 	}
 
 	public void testMethod(List<User> users) {
 		 List<String> result = users.stream() // ここでリストusersからstreamオブジェクトを取得
 		 .map( // 写像開始
 		   (user) ->  { // ラムダという書き方で、この中の関数に引数として渡すuserの型や返値の型などを省略できる書き方
-		     return user.get();
+		     return user.getName();
 		   }
 		 )
 		 .collect(Collectors.toList()); // この行はおまじない。最後にstreamから扱いやすいListに戻してあげる「終端操作」というやつ。javaはここが弱い。
---
+++ src/User.java
@@ -0,0 +1,30 @@
+
+public class User {
+
+	private int id;
+	private String name;
+
+	public User(int id, String name) {
+		this.setId(id);
+		this.setName(name);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+}
