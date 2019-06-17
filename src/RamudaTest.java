80f1bb4bf4d39b17aced56440778528070a40cc3
takeshi <takagitakeshi2@gmail.com>
2019-06-18 00:23:41 +0900

    second commit

--- src/RamudaTest.java
+++ src/RamudaTest.java
@@ -16,24 +16,36 @@ public class RamudaTest {
 		users.add(user2);
 		users.add(user3);
 
-		for(User user : users) {
+		for (User user : users) {
 			System.out.print(user.getId() + "：");
 			System.out.println(user.getName());
 		}
 
-		testMethod(User users);
+		System.out.println("");
+
+		testMethod1(users);
+		testMethod2(users);
 
 	}
 
-	public void testMethod(List<User> users) {
-		 List<String> result = users.stream() // ここでリストusersからstreamオブジェクトを取得
-		 .map( // 写像開始
-		   (user) ->  { // ラムダという書き方で、この中の関数に引数として渡すuserの型や返値の型などを省略できる書き方
-		     return user.getName();
-		   }
-		 )
-		 .collect(Collectors.toList()); // この行はおまじない。最後にstreamから扱いやすいListに戻してあげる「終端操作」というやつ。javaはここが弱い。
-		 System.out.println(result); // コンソールに [佐藤A男, 鈴木B子, 高橋C助] とひょうじされるはず。
-		}
+	public static void testMethod1(List<User> users) {
+		List<Integer> result = users.stream() // ここでリストusersからstreamオブジェクトを取得
+				.map( // 写像開始
+						(user) -> { // ラムダという書き方で、この中の関数に引数として渡すuserの型や返値の型などを省略できる書き方
+							return user.getId();
+						})
+				.collect(Collectors.toList()); // この行はおまじない。最後にstreamから扱いやすいListに戻してあげる「終端操作」というやつ。javaはここが弱い。
+		System.out.println(result); // コンソールに [佐藤A男, 鈴木B子, 高橋C助] とひょうじされるはず。
+	}
+
+	public static void testMethod2(List<User> users) {
+		List<String> result = users.stream() // ここでリストusersからstreamオブジェクトを取得
+				.map( // 写像開始
+						(user) -> { // ラムダという書き方で、この中の関数に引数として渡すuserの型や返値の型などを省略できる書き方
+							return user.getName();
+						})
+				.collect(Collectors.toList()); // この行はおまじない。最後にstreamから扱いやすいListに戻してあげる「終端操作」というやつ。javaはここが弱い。
+		System.out.println(result); // コンソールに [佐藤A男, 鈴木B子, 高橋C助] とひょうじされるはず。
+	}
 
 }
